6e32549593ce6b3c13e2da9597369ae7d2a09dd8
Areeba Khan <kahnareeba@gmail.com>
2022-01-18 19:30:30 -0600

    Adds README on api and a POST request to send an image

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
-# IMAGE_LINK = 'https://media.discordapp.net/attachments/860750045472358450/933063826419638352/20201031_110739.jpg?width=193&height=407'
-# r = requests.get(IMAGE_LINK)
+from flask import Flask, request
+import requests
 
-# with open('google_logo.png', 'wb') as f:
-#     f.write(r.content)
-
-from flask import Flask
+# other
+import re
+import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-@app.route("/bye")
-def bye():
-    return "<p>Goodbye, World!</p>"
+@app.route("/image", methods=['POST'])
+def image():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        user_id = request_data['userID']
+        image_url = request_data['imageURL']
+        time = re.sub(r"[,.:;@#?!&$]+\ *", "-", datetime.now())
 
+        r = requests.get(image_url)
+        with open(user_id + "-" + time + '.png', 'wb') as f:
+            f.write(r.content)
+        
+        return "IMAGE RECIEVED BY USER " + user_id
+    else:
+        return '''
+        ERROR: Requesting @ route /image with method other than POST.
+            Only post method is supported. 
+            Upon using post, the resulting image will be sent back.
+        '''
 
 if __name__ == '__main__':
    app.run(debug = True)
