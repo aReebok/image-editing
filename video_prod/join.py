c03ad41142eac9eaba0a62712b7918a70d1f217c
Areeba Khan <kahnareeba@gmail.com>
2022-01-20 22:56:10 -0600

    Adds all the splititng joining processes together

--- video_prod/join.py
+++ video_prod/join.py
@@ -5,23 +5,22 @@ import shutil
 # code from
 # https://www.programcreek.com/python/example/72134/cv2.VideoWriter
 
-video_name = '08_output.mp4'
-image_folder = 'output_backup'
+def join():
+    video_name = '08_output.mp4'
+    image_folder = 'output_backup'
 
-images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
 
-frame = cv2.imread(os.path.join(image_folder, images[0]))
-height, width, layers = frame.shape
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
 
-# video = cv2.VideoWriter(video_name, fourcc, 25, (width,height))
-video = cv2.VideoWriter(video_name, 0x00000021, 25, (width,height))
+    # video = cv2.VideoWriter(video_name, fourcc, 25, (width,height))
+    video = cv2.VideoWriter(video_name, 0x00000021, 25, (width,height))
 
-count = 0
-for image in images:
-    # if count % 3 == 0:
-    video.write(cv2.imread(os.path.join(image_folder, image)))
-# else:
-#         pass
+    count = 0
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
 
-cv2.destroyAllWindows()
-video.release()
+
+    cv2.destroyAllWindows()
+    video.release()
---
+++ video_prod/produce.py
@@ -0,0 +1,7 @@
+from split import * 
+from run import *
+from join import *
+
+split()
+run()
+join()
