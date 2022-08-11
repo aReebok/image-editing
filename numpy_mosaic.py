33bc01028d389638f2b80d34c307ecac0f93895a
= <=>
2022-08-10 21:53:03 -0500

    Creates 5D array for input image splitted into 20x20 dim

--- numpy_mosaic.py
+++ numpy_mosaic.py
@@ -4,36 +4,34 @@ import numpy as np
 from skimage import (io)
 from time import time
 
-def time_np(pictures):
-    start = time()
+def create_rgb_small_img_list(pictures):
+    rgb_small_imgs = []
     for i in range(len(pictures)):
-        print_img_info_vectorized(pictures[i])
-    stop = time()
-    print("NP vectorized code took time of " + str(stop-start) + " s")
+        rgb_small_imgs.append(rgb_small_img_vectorized(pictures[i]))
+    return np.array(rgb_small_imgs)
 
+def rgb_small_img_vectorized(img):
+    return np.array(img).mean(axis=(0,1), dtype=int)
 
-def time_it(pictures):
-    start = time()
-    for i in range(len(pictures)):
-        print_img_info_iteratively(pictures[i])
-    stop = time()
-    print("Iterative code took time of " + str(stop-start) + " s")
+def blockshaped(img_path, kernel_size: tuple):
+    img = io.imread(img_path)
+    h, w, c = img.shape
+    tile_h, tile_w = kernel_size
 
-def print_img_info_vectorized(img):
-    avg_rgb = np.array(img).mean(axis=(0,1), dtype=int)
+    tiled_arr = img.reshape(h // tile_h, tile_h,
+                            w // tile_w, tile_w,
+                            c)
+    tiled_arr = tiled_arr.swapaxes(1,2)
+    return tiled_arr
 
-def print_img_info_iteratively(img):
-    rgb_avg = []
-    for k in range(2,-1,-1):
-        sum = 0
-        for i in range(20):
-            for j in  range (20):
-                sum += img[i][j][k]
-        rgb_avg.append(int(sum/400))
+small_images_path = './images/*.jpg'
+input_image_path = 'lion.jpg'
+pictures = io.imread_collection(small_images_path)
 
 if __name__ == '__main__':
-    path = './images/*.jpg'
-    pictures = io.imread_collection(path)
+    a = create_rgb_small_img_list(pictures)
+    b = blockshaped(input_image_path, (20,20))
+    print(a.shape)
+    print(b.shape)
 
-    time_it(pictures)
-    time_np(pictures)
+
