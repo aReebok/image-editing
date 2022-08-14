529bd228c1ccd12dbb2ed2f4fbc6573594877caa
Areeba Khan <kahnareeba@gmail.com>
2022-08-13 23:12:31 -0500

    Cleans code

--- numpy_mosaic.py
+++ numpy_mosaic.py
@@ -3,7 +3,6 @@
 import numpy as np
 from skimage import (io)
 from time import time
-from numpy import savetxt
 
 def create_rgb_small_img_list(pictures):
     rgb_small_imgs = []
@@ -13,7 +12,6 @@ def create_rgb_small_img_list(pictures):
 
 def get_rgb_small_img(img):
     return np.array(img).mean(axis=(0,1), dtype=int)
-
 
 def unblockshaped(img_arr_5d):
     h, w, t_h, t_w, c = img_arr_5d.shape
@@ -38,32 +36,20 @@ def avg_rgb_channels_5D_arr(arr_5d):
 def trunc_rgb(arr, fac):
     return np.around(arr*fac).astype(int)
 
-# def write_out_small_imgs(a):
-#     c = trunc_rgb(a, 0.1)
-#     h, c = c.shape
-#     d = np.stack((np.arange(start=1, stop=h), c))
-#     # print(d.shape)
-#     np.savetxt('rgb_small_img.txt', trunc_rgb(a, 0.1), fmt='%d')
-
-small_images_path = './images/*.jpg'
-input_image_path = 'lion.jpg'
-pictures = io.imread_collection(small_images_path)
-
 def closest(colors, target):
     distances = np.sqrt(np.sum((colors-target)**2, axis=1))
     index_of_smallest = np.where(distances==np.amin(distances))
-    # smallest_distance = colors[index_of_smallest]
     return index_of_smallest[0][0]
 
-
-
-
-if __name__ == '__main__':
+def run():
+    small_images_path = './images/*.jpg'
+    input_image_path = 'lion.jpg'
+    pictures = io.imread_collection(small_images_path)
     a = create_rgb_small_img_list(pictures)
     b = blockshaped(input_image_path, (20, 20))
     c = avg_rgb_channels_5D_arr(b)
 
-    #map
+    # map
     height, width, channels = c.shape
 
     for i in range(height):
@@ -72,3 +58,11 @@ if __name__ == '__main__':
 
     d = unblockshaped(b)
     io.imsave('testlion.jpg', d)
+
+
+if __name__ == '__main__':
+    start = time()
+    run()
+    stop = time()
+    print("Time NP usage: " + str(stop - start))
+
